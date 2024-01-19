d80e00ad67c832ed685ab57993696a05c308ca34
Raxuis <raclot.raphael@gmail.com>
2024-01-19 16:40:42 +0100

    Edited : homepage / Added : images

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -1,1 +1,27 @@
-<p>Test</p>
+<h3>Page d'accueil</h3>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <img src="./assets/burgers/burger_1.jpg" alt="burger_1">
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <img src="./assets/pizzas/pizza_1.jpg" alt="pizza_1">
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
+</div>
