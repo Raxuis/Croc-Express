988378a48b71b5be0b0ddfa8c64c656059e63034
Raxuis <raclot.raphael@gmail.com>
2024-01-19 17:01:30 +0100

    Stylized : homepage

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -5,11 +5,13 @@
             <img src="./assets/burgers/burger_1.jpg" alt="burger_1">
         </div>
         <div class="card-body">
+            <h3>Burger</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
                 deserunt mollit anim id est laborum.</p>
+            <button class="other-products">Tous nos produits<i class="fa-solid fa-arrow-right"></i></button>
         </div>
     </div>
     <div class="card">
@@ -17,11 +19,13 @@
             <img src="./assets/pizzas/pizza_1.jpg" alt="pizza_1">
         </div>
         <div class="card-body">
+            <h3>Pizza</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
                 deserunt mollit anim id est laborum.</p>
+            <button class="other-products">Tous nos produits<i class="fa-solid fa-arrow-right"></i></button>
         </div>
     </div>
 </div>
