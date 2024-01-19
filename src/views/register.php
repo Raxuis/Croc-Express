f2d035a2fbe500485357f8b7c48ca3b4e7a3e82b
Raxuis <raclot.raphael@gmail.com>
2024-01-19 15:18:59 +0100

    Edited register form

--- src/views/register.php
+++ src/views/register.php
@@ -1,13 +1,15 @@
-<form action="" method="post">
+<form action="" method="post" id="register">
     <label for="firstname">
-        First name:
+        Prénom:
     </label>
     <input type="text" name="firstname" id="firstname">
-    <label for="lastname">Last name :</label>
+    <label for="lastname">Nom :</label>
     <input type="text" name="lastname" id="lastname">
     <label for="email">Email: </label>
     <input type="email" name="email" id="email">
-    <label for="password">Password: </label>
+    <label for="password">Mot de passe: </label>
     <input type="password" name="password" id="password">
-    <input type="submit" value="Submit">
+    <label for="password_confirmation">Mot de passe de confirmation: </label>
+    <input type="password" name="password_confirmation" id="password_confirmation">
+    <input type="submit" value="Submit" class="submit">
 </form>
