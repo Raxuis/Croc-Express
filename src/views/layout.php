b5808b10646df91e207c7f4a56c4a09dbf68dd56
Raxuis <raclot.raphael@gmail.com>
2024-01-19 11:25:37 +0100

    Added / Stylized : footer

--- src/views/layout.php
+++ src/views/layout.php
@@ -12,6 +12,11 @@
     <main>
         <?php require '../src/controllers/' . $route . '_controller.php'; ?>
     </main>
+    <footer>
+        <div id="footer-container">
+            <p id="copyrights">© 2024 Raphaël | Benoît CODA, Orléans. All Rights Reserved</p>
+        </div>
+    </footer>
 </body>
 
 </html>
