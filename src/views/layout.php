0a86ad5c02541a35f05d26a5c0db3653ab678ce2
Raxuis <raclot.raphael@gmail.com>
2024-01-19 12:02:36 +0100

    Stylized : navbar

--- src/views/layout.php
+++ src/views/layout.php
@@ -6,9 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Croc Express</title>
     <link rel="stylesheet" href="../src/styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
+    <header>
+        <nav>
+            <a href="?page=homepage"><img src="./assets/favicon.ico"></a>
+            <ul>
+                <li><a href="?page=admin">Administration</a></li>
+                <li><a href="?page=profile">User</a></li>
+                <li><a href="?page=contact">Contactez-nous</a></li>
+                <li><a href="?page=cart"><img src=""></a></li>
+            </ul>
+        </nav>
+    </header>
     <main>
         <?php require '../src/controllers/' . $route . '_controller.php'; ?>
     </main>
@@ -17,6 +33,9 @@
             <p id="copyrights">© 2024 Raphaël | Benoît, CODA-Orléans. All Rights Reserved</p>
         </div>
     </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
