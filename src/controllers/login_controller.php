309abb9821c67b44d7d7f21550a6b6bf6790893d
Raxuis <raclot.raphael@gmail.com>
2024-01-19 16:01:42 +0100

    Edited forms

--- src/controllers/login_controller.php
+++ src/controllers/login_controller.php
@@ -1,1 +1,2 @@
 <?php
+require '../src/views/login.php';
--- src/views/register.php
+++ src/views/register.php
@@ -1,15 +1,16 @@
-<form action="" method="post" id="register">
+<h3>Page de création de compte</h3>
+<form action="" method="post" class="form">
     <label for="firstname">
         Prénom:
     </label>
-    <input type="text" name="firstname" id="firstname">
+    <input type="text" name="firstname" id="firstname" placeholder="Elliot">
     <label for="lastname">Nom :</label>
-    <input type="text" name="lastname" id="lastname">
+    <input type="text" name="lastname" id="lastname" placeholder="Alderson">
     <label for="email">Email: </label>
-    <input type="email" name="email" id="email">
+    <input type="email" name="email" id="email" placeholder="elliot@fsociety.com">
     <label for="password">Mot de passe: </label>
-    <input type="password" name="password" id="password">
+    <input type="password" name="password" id="password" placeholder="darlene1234">
     <label for="password_confirmation">Mot de passe de confirmation: </label>
-    <input type="password" name="password_confirmation" id="password_confirmation">
-    <input type="submit" value="Submit" class="submit">
+    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="darlene1234">
+    <button type="submit" value="register" class="submit">Créer un compte</button>
 </form>
