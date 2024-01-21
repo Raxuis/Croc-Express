42e335aeece523ee60a5168ca1921ac5c294e6e6
Raxuis <raclot.raphael@gmail.com>
2024-01-21 15:34:37 +0100

    Stylized forms

--- src/views/login.php
+++ src/views/login.php
@@ -1,10 +1,12 @@
 <h3>Page de connexion</h3>
-<form action="" method="post" class="form">
-    <label for="email">Entrez votre email : </label>
-    <input type="email" id="email" name="email" autofocus placeholder="Email" id="username" required />
+<div class="container">
+    <form action="" method="post" class="form">
+        <label for="email">Entrez votre email : </label>
+        <input type="email" id="email" name="email" autofocus placeholder="Email" id="username" required />
 
-    <label for="password">Entrez votre mot de passe : </label>
-    <input type="password" id="password" name="password" placeholder="Password" id="password" required />
-    <!-- <input type="hidden" name="token" value="php echo $_SESSION['token'] ?? '' "> -->
-    <button type="submit" value="login" class="submit">Se connecter</button>
-</form>
+        <label for="password">Entrez votre mot de passe : </label>
+        <input type="password" id="password" name="password" placeholder="Password" id="password" required />
+        <!-- <input type="hidden" name="token" value="php echo $_SESSION['token'] ?? '' "> -->
+        <button type="submit" value="login" class="submit">Se connecter</button>
+    </form>
+</div>
--- src/views/register.php
+++ src/views/register.php
@@ -1,16 +1,19 @@
 <h3>Page de création de compte</h3>
-<form action="" method="post" class="form">
-    <label for="firstname">
-        Prénom:
-    </label>
-    <input type="text" name="firstname" id="firstname" placeholder="Elliot">
-    <label for="lastname">Nom :</label>
-    <input type="text" name="lastname" id="lastname" placeholder="Alderson">
-    <label for="email">Email: </label>
-    <input type="email" name="email" id="email" placeholder="elliot@fsociety.com">
-    <label for="password">Mot de passe: </label>
-    <input type="password" name="password" id="password" placeholder="darlene1234">
-    <label for="password_confirmation">Mot de passe de confirmation: </label>
-    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="darlene1234">
-    <button type="submit" value="register" class="submit">Créer un compte</button>
-</form>
+<div class="container">
+
+    <form action="" method="post" class="form">
+        <label for="firstname">
+            Prénom:
+        </label>
+        <input type="text" name="firstname" id="firstname" placeholder="Elliot">
+        <label for="lastname">Nom :</label>
+        <input type="text" name="lastname" id="lastname" placeholder="Alderson">
+        <label for="email">Email: </label>
+        <input type="email" name="email" id="email" placeholder="elliot@fsociety.com">
+        <label for="password">Mot de passe: </label>
+        <input type="password" name="password" id="password" placeholder="darlene1234">
+        <label for="password_confirmation">Mot de passe de confirmation: </label>
+        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="darlene1234">
+        <button type="submit" value="register" class="submit">Créer un compte</button>
+    </form>
+</div>
