ff96d8071d30c65f79e468b5a178978f3e47fae3
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-22 15:44:15 +0100

    added admin verif

--- src/controllers/admin_add_product_controller.php
+++ src/controllers/admin_add_product_controller.php
@@ -1,5 +1,9 @@
 <?php
 
+if(!isset($_SESSION["is_admin"])) {
+    header('location: index.php');
+    exit(0);
+}
 
 $categoriesManager = new CategoryManager($bdd, "categories");
 $categories = $categoriesManager->getAll();
