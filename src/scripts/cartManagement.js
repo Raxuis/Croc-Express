4d0b61da6fb776cbe125189e85f7d817683c8d84
Raxuis <raclot.raphael@gmail.com>
2024-01-24 16:16:40 +0100

    Edited : cartManagement.js (removed unused functions)

--- src/scripts/cartManagement.js
+++ src/scripts/cartManagement.js
@@ -1,26 +1,23 @@
 function updateCartValue(cart, value) {
-    cart.innerHTML = value;
+  cart.innerHTML = value;
 }
 
 function addToCart(cart, product) {
-    // TODO : Modifier ça
-
-    fetch("../src/controllers/add_to_cart.php?id="+product).then((response) => {
-        // return response.json();
-        response.json().then((data) => {
-            console.log(data);
-            updateCartValue(cart, data.total)
-        });
+  fetch("../src/controllers/add_to_cart.php?id=" + product).then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+      updateCartValue(cart, data.total);
     });
+  });
 }
 function buttonListener(product, button, cart) {
-    button.addEventListener("click", () => {
-        addToCart(cart, product);
-    });
+  button.addEventListener("click", () => {
+    addToCart(cart, product);
+  });
 }
 
 function initializeCart(productId, buttonId, cart) {
-    const product = productId;
-    const button = document.getElementById(buttonId);
-    buttonListener(product, button, cart);
+  const product = productId;
+  const button = document.getElementById(buttonId);
+  buttonListener(product, button, cart);
 }
