48e1d2bc7b2a748708757ec6d975c130abce3499
Raxuis <raclot.raphael@gmail.com>
2024-01-25 15:53:23 +0100

    Feat(front) : style contact page

--- src/views/contact.php
+++ src/views/contact.php
@@ -1,19 +1,20 @@
 <h3>Nous contacter</h3>
+<div class="container">
+    <form action="" method="post" class="form">
 
-<form action="" method="post" class="form">
+        <label for="title">Titre de votre message</label>
+        <input type="text" name="title" placeholder="Titre">
 
-    <label for="title">Titre de votre message</label>
-    <input type="text" name="title" placeholder="Titre">
+        <label for="content">Votre message</label>
+        <textarea name="content" placeholder="Contenu"></textarea>
 
-    <label for="content">Votre message</label>
-    <textarea name="content" placeholder="Contenu"></textarea>
+        <input type="hidden" name="userId" value="<?= $_SESSION["user_id"] ?>">
+        <input type="hidden" name="ip" value="<?= $_SERVER["REMOTE_ADDR"] ?>">
 
-    <input type="hidden" name="userId" value="<?= $_SESSION["user_id"] ?>">
-    <input type="hidden" name="ip" value="<?= $_SERVER["REMOTE_ADDR"] ?>">
-
-    <?php if(isset($_SESSION["user_id"])) { ?>
-        <button type="submit" class="submit">Envoyer</button>
-    <?php } else { ?>
-        <p>Connectez-vous pour envoyer ce message</p>
-    <?php } ?>
-</form>
+        <?php if (isset($_SESSION["user_id"])) { ?>
+            <button type="submit" value="register" class="submit">Envoyer</button>
+        <?php } else { ?>
+            <p>Connectez-vous pour envoyer ce message</p>
+        <?php } ?>
+    </form>
+</div>
