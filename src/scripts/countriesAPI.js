fff1f97b2f897614022b59cffe2cbba2b148ffa1
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-26 14:29:42 +0100

    fix(front) selected france by default

--- src/scripts/countriesAPI.js
+++ src/scripts/countriesAPI.js
@@ -1,7 +1,19 @@
 function getAllCountries() {
     return fetch('https://restcountries.com/v3.1/all')
         .then((response) => response.json())
-        .then((data) => data);
+        .then((data) => {
+            data.sort(function (a, b) {
+                if (a.translations.fra.common < b.translations.fra.common) {
+                    return -1;
+                }
+                if (a.translations.fra.common > b.translations.fra.common) {
+                    return 1;
+                }
+                return 0;
+            })
+
+            return data;
+        });
 }
 
 function putCountriesInSelect(countries, select) {
@@ -9,6 +21,7 @@ function putCountriesInSelect(countries, select) {
         const option = document.createElement('option');
         option.value = country.translations.fra.common;
         option.textContent = country.translations.fra.common;
+        if(country.translations.fra.common === "France") option.setAttribute("selected", "selected")
         select.appendChild(option);
     });
 }
