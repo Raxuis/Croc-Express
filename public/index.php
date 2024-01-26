7562488a2b6707ceb12686bd00874936c71d93f5
Raxuis <raclot.raphael@gmail.com>
2024-01-26 11:15:43 +0100

    Feat(back) : payment

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ if (isset($_GET['page']) && $_GET['page'] == "disconnect") {
     header("Location: " . BASE_PATH);
 
 }
-$availableRoutes = ['homepage', 'register', 'login', 'cart', 'show_products', 'contact', 'edit_profile', 'orders', 'admin_messages', 'admin_add_food', 'admin_add_product', 'admin_edit_product', 'admin_add_menu', 'admin_products', 'admin_orders', 'admin_bestsellers', 'admin_turnover', 'admin_deliver', 'admin_add_category', 'admin_coupons', 'admin_menus'];
+$availableRoutes = ['homepage', 'register', 'login', 'cart', 'show_products', 'contact', 'edit_profile', 'orders', 'admin_messages', 'admin_add_food', 'admin_add_product', 'admin_edit_product', 'admin_add_menu', 'admin_products', 'admin_orders', 'admin_bestsellers', 'admin_turnover', 'admin_deliver', 'admin_add_category', 'admin_coupons', 'admin_menus', 'payment'];
 $route = 'homepage';
 if (isset($_GET['page']) && in_array($_GET['page'], $availableRoutes)) {
     $route = $_GET['page'];
