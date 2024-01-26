53190b8b4e595d23b1a7c83eb55d83d68c72c248
Raxuis <raclot.raphael@gmail.com>
2024-01-26 16:54:16 +0100

    Feat(full) : verification to avoid hidden categories

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -1,18 +1,20 @@
 <h3>Page d'accueil</h3>
 <div class="container">
     <?php foreach ($categories as $category): ?>
-        <div class="card">
-            <div class="card-body">
-                <h3>
-                    <?= $category['name'] ?>
-                </h3>
-                <p>
-                    <?= $category['description'] ?>
-                </p>
-                <button class="orange-button"
-                    onclick="window.location.href='?page=show_products&category_id=<?= $category['id'] ?>'">Tous nos
-                    produits<i class="fa-solid fa-arrow-right"></i></button>
+        <?php if ($category['is_hidden'] === 0) { ?>
+            <div class="card">
+                <div class="card-body">
+                    <h3>
+                        <?= $category['name'] ?>
+                    </h3>
+                    <p>
+                        <?= $category['description'] ?>
+                    </p>
+                    <button class="orange-button"
+                        onclick="window.location.href='?page=show_products&category_id=<?= $category['id'] ?>'">Tous nos
+                        produits<i class="fa-solid fa-arrow-right"></i></button>
+                </div>
             </div>
-        </div>
+        <?php } ?>
     <?php endforeach; ?>
 </div>
