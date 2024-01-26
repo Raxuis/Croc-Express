73fa0f1a922485f30f88eff207cf40757c93b9b3
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-26 11:53:39 +0100

    feat(global) added crud for reduction

--- src/controllers/admin_coupons_controller.php
+++ src/controllers/admin_coupons_controller.php
@@ -1,2 +1,11 @@
 <?php
+
+if (isset($_POST['delete'])) {
+    $couponManager->deleteOne($_POST['id']);
+    header("location: index.php?page=admin_coupons");
+    exit;
+}
+
+$allCoupons = $couponManager->getAll();
+
 require PATH_VIEWS . 'admin_coupons.php';
---
+++ src/views/admin_add_coupon.php
@@ -0,0 +1,9 @@
+<h3>Ajouter un coupon de réduction</h3>
+
+<div class="container">
+    <form action="" method="post" class="form">
+        <input type="text" name="name" placeholder="Nom du coupon">
+        <input type="number" name="reduction" min="0" placeholder="Réduction">
+        <button class="submit" type="submit">Ajouter ce coupon</button>
+    </form>
+</div>
