dd1a98a41a5814cc9774854e9323ebac85b8c498
Raxuis <raclot.raphael@gmail.com>
2024-01-29 14:57:43 +0100

    Feat(full) : download pdf of orders

--- src/controllers/orders_controller.php
+++ src/controllers/orders_controller.php
@@ -1,11 +1,41 @@
 <?php
-$orders = $orderManager->getOrdersByUserId($_SESSION['user_id']);
+require '../vendor/autoload.php';
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
-if (count($orders) < 1) {
-    $_SESSION['status'] = 'error';
-    $_SESSION['message'] = 'Vous n\'avez pas encore de commandes';
-    header('Location:index.php');
-    exit();
+if (isset($_GET['page'])) {
+    if ($_GET['page'] == 'orders' && !isset($_GET['order_id'])) {
+        $orders = $orderManager->getOrdersByUserId($_SESSION['user_id']);
+
+        if (count($orders) < 1) {
+            $_SESSION['status'] = 'error';
+            $_SESSION['message'] = 'Vous n\'avez pas encore de commandes';
+            header('Location:index.php');
+            exit();
+        }
+    } else if ($_GET['page'] == 'orders' && isset($_GET['order_id'])) {
+        $order = $orderManager->getOne($_GET['order_id']);
+        $products = $orderProductManager->getProductsOfOrder($order['id']);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['getPdf'])) {
+            try {
+                ob_start();
+                /* $_GET['order_id'] = $_POST['getPdf']; */
+                include PATH_VIEWS . 'orders.php';
+                $content = ob_get_clean();
+                ob_end_clean();
+                $options = new Options();
+                $options->set('defaultFont', 'Courier');
+                $dompdf = new Dompdf($options);
+                $dompdf->setPaper('A4', 'portrait');
+                $dompdf->loadHtml($content);
+                $dompdf->render();
+                $dompdf->stream('order_n°' . $_GET['order_id']);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+
+
+        }
+    }
 }
-
 require PATH_VIEWS . 'orders.php';
--- src/views/orders.php
+++ src/views/orders.php
@@ -1,11 +1,84 @@
 <h3>Mes commandes</h3>
 
 <div class="container">
-    <?php foreach ($orders as $order) {
+    <?php if (!isset($_GET['order_id'])) { ?>
+        <?php foreach ($orders as $order) {
+            $coupon = $orderManager->getCouponOfOrder($order["id"]);
+            $coupon = $coupon ? $coupon[0] : null;
+            ?>
+            <div class="card-orders">
+                <div class="card-header">
+                    <h3>Commande n°
+                        <?= $order['id'] ?>
+                    </h3>
+                    <p>Passée le
+                        <?= $order['created_at'] ?>
+                    </p>
+                    <p>Montant total :
+                        <?= $order['price'] ?> €
+                    </p>
+                    <p>En livraison :
+                        <?= $order['is_in_delivery'] ? "Oui" : "Non" ?>
+                    </p>
+                </div>
+                <div class="card-body">
+                    <h3>Produits</h3>
+                    <table class='table-cart'>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Produit</th>
+                                <th>Prix Unitaire</th>
+                                <th>Quantité</th>
+                                <th>Prix Total</th>
+                            </tr>
+                        </thead>
+                        <?php
+                        $products = $orderProductManager->getProductsOfOrder($order['id']);
+                        $productsIds = [];
+                        foreach ($products as $product) {
+                            if (!in_array($product['id'], $productsIds)) {
+                                $productsIds[] = $product['id'];
+                            } else {
+                                continue;
+                            }
+
+                            ?>
+                            <tr>
+                                <td class="td-images">
+                                    <img src="<?= PATH_IMAGES . $product['image'] ?>" alt="" class='cart-images'>
+                                </td>
+                                <td>
+                                    <?= $product['name'] ?>
+                                </td>
+                                <td>
+                                    <?= $product['price'] . '€' ?>
+                                </td>
+                                <td>
+                                    <?= $product['quantity'] ?>
+                                </td>
+                                <td>
+                                    <?= $product['total_price'] . "€" ?>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                    <p>Code promotionnel :
+                        <?= $coupon ? $coupon["name"] . " (-" . $coupon["reduction"] . "%)" : "Aucun code utilisé" ?>
+                    </p>
+                    <a href="index.php?page=orders&order_id=<?= $order['id'] ?>">Plus d'informations<i
+                            class="fa-solid fa-circle-info"></i></a>
+                </div>
+            </div>
+        <?php } ?>
+    <?php } else { ?>
+        <?php
         $coupon = $orderManager->getCouponOfOrder($order["id"]);
         $coupon = $coupon ? $coupon[0] : null;
-        ?>
-        <div class="card-orders">
+        if (!isset($_POST['getPdf'])) { ?>
+            <a href="index.php?page=orders"><i class="fa-solid fa-backward"></i>Retour à mes commandes</a>
+        <?php } ?>
+        <div class="card-orders" id="order">
             <div class="card-header">
                 <h3>Commande n°
                     <?= $order['id'] ?>
@@ -25,7 +98,9 @@
                 <table class='table-cart'>
                     <thead>
                         <tr>
-                            <th></th>
+                            <?php if (!isset($_POST['getPdf'])) { ?>
+                                <th></th>
+                            <?php } ?>
                             <th>Produit</th>
                             <th>Prix Unitaire</th>
                             <th>Quantité</th>
@@ -41,12 +116,13 @@
                         } else {
                             continue;
                         }
-
                         ?>
                         <tr>
-                            <td class="td-images">
-                                <img src="<?= PATH_IMAGES . $product['image'] ?>" alt="" class='cart-images'>
-                            </td>
+                            <?php if (!isset($_POST['getPdf'])) { ?>
+                                <td class="td-images">
+                                    <img src="<?= PATH_IMAGES . $product['image'] ?>" alt="" class='cart-images'>
+                                </td>
+                            <?php } ?>
                             <td>
                                 <?= $product['name'] ?>
                             </td>
@@ -67,5 +143,12 @@
                 </p>
             </div>
         </div>
-    <?php } ?>
+        <?php if (!isset($_POST['getPdf'])) { ?>
+            <div>
+                <form method='post' action=''>
+                    <button type="submit pay" name="getPdf" class="submit" id='pdf'>Télécharger la facture en PDF</button>
+                </form>
+            </div>
+        <?php }
+    } ?>
 </div>
