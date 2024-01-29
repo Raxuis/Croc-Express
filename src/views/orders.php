aecd5b1f33cbfe86223c27cb13295bcbb44c0807
Raxuis <raclot.raphael@gmail.com>
2024-01-29 09:12:33 +0100

    Fix(front) : card from cart sizes

--- src/views/orders.php
+++ src/views/orders.php
@@ -5,12 +5,20 @@
         $coupon = $orderManager->getCouponOfOrder($order["id"]);
         $coupon = $coupon ? $coupon[0] : null;
         ?>
-        <div class="card">
+        <div class="card-orders">
             <div class="card-header">
-                <h3>Commande n°<?= $order['id'] ?></h3>
-                <p>Passée le <?= $order['created_at'] ?></p>
-                <p>Montant total : <?= $order['price'] ?> €</p>
-                <p>En livraison : <?= $order['is_in_delivery'] ? "Oui" : "Non" ?> </p>
+                <h3>Commande n°
+                    <?= $order['id'] ?>
+                </h3>
+                <p>Passée le
+                    <?= $order['created_at'] ?>
+                </p>
+                <p>Montant total :
+                    <?= $order['price'] ?> €
+                </p>
+                <p>En livraison :
+                    <?= $order['is_in_delivery'] ? "Oui" : "Non" ?>
+                </p>
             </div>
             <div class="card-body">
                 <h3>Produits</h3>
@@ -28,7 +36,7 @@
                     $products = $orderProductManager->getProductsOfOrder($order['id']);
                     $productsIds = [];
                     foreach ($products as $product) {
-                        if(!in_array($product['id'], $productsIds)) {
+                        if (!in_array($product['id'], $productsIds)) {
                             $productsIds[] = $product['id'];
                         } else {
                             continue;
@@ -54,7 +62,10 @@
                         </tr>
                     <?php } ?>
                 </table>
-                <p>Code promotionnel : <?= $coupon ? $coupon["name"] . " (-" . $coupon["reduction"] . "%)" : "Aucun code utilisé" ?></p>
+                <p>Code promotionnel :
+                    <?= $coupon ? $coupon["name"] . " (-" . $coupon["reduction"] . "%)" : "Aucun code utilisé" ?>
+                </p>
             </div>
         </div>
     <?php } ?>
+</div>
