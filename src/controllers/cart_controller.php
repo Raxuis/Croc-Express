a5e10ee7f12163f55c9ba89e4d7ed9ec4a3ddb8a
Raxuis <raclot.raphael@gmail.com>
2024-01-29 16:10:28 +0100

    Fix(back) : verif if inputs are empty

--- src/controllers/cart_controller.php
+++ src/controllers/cart_controller.php
@@ -1,9 +1,9 @@
 <?php
 $productsManager = new ProductManager($bdd, 'products');
 $productImageManager = new ProductImageManager($bdd, 'product_image');
-
+$_SESSION['inDelivery'] = false;
 if (!empty($_POST)) {
-    if(!(isset($_POST['token']) && $_POST['token'] == $_SESSION['token'])) {
+    if (!(isset($_POST['token']) && $_POST['token'] == $_SESSION['token'])) {
         exit;
     }
 }
--- src/controllers/payment_controller.php
+++ src/controllers/payment_controller.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION["user_id"])) {
     exit(0);
 }
 
-if (!empty($_POST) && !empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['address'] && !empty($_POST['city']) && !empty($_POST['zip']) && !empty($_POST['country']))) {
+if (!empty($_POST)) {
     if (isset($_POST['token']) && $_POST['token'] == $_SESSION['token']) {
         $cart = $_SESSION['cart'];
         $totalPrice = 0;
@@ -15,75 +15,77 @@ if (!empty($_POST) && !empty($_POST['firstname']) && !empty($_POST['lastname']) 
 
         $inDelivery = $_SESSION['inDelivery'] ?? false;
         $addressId = null;
+        echo $_SESSION['inDelivery'];
+        if (!empty($_SESSION['inDelivery']) && $_SESSION['inDelivery'] === true) {
+            if (!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['address'] && !empty($_POST['city']) && !empty($_POST['zip']) && !empty($_POST['country']))) {
+                $firstname = $_POST['firstname'];
+                $lastname = $_POST['lastname'];
+                $street = $_POST['address'];
+                $city = $_POST['city'];
+                $zipCode = $_POST['zip'];
+                $country = $_POST['country'][0];
 
-        if (isset($_SESSION['inDelivery']) && $_SESSION['inDelivery'] === true) {
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
-            $street = $_POST['address'];
-            $city = $_POST['city'];
-            $zipCode = $_POST['zip'];
-            $country = $_POST['country'][0];
-
-            $address = new Address([
-                'userId' => $_SESSION['user_id'],
-                'street' => $street,
-                'city' => $city,
-                'zip' => $zipCode,
-                'country' => $country
-            ]);
-            $addressId = $addressManager->createOne($address);
-        }
-
-        var_dump($_POST['coupon']);
-
-        $coupon = null;
-        if (!empty($_POST['coupon'])) {
-            $coupon = $couponManager->getOneByName($_POST['coupon']);
-
-            if ($coupon) {
-                $totalPrice -= $totalPrice * ($coupon['reduction'] / 100);
+                $address = new Address([
+                    'userId' => $_SESSION['user_id'],
+                    'street' => $street,
+                    'city' => $city,
+                    'zip' => $zipCode,
+                    'country' => $country
+                ]);
+                $addressId = $addressManager->createOne($address);
             } else {
                 $_SESSION['status'] = "error";
-                $_SESSION['message'] = "Le coupon n'existe pas";
+                $_SESSION['message'] = "Veuillez remplir tous les champs";
                 ob_clean();
                 header("location: index.php?page=cart");
                 exit;
             }
         }
-
-        $order = new Order([
-            'userId' => $_SESSION['user_id'],
-            'price' => $totalPrice,
-            'couponId' => $coupon ? $coupon['id'] : null,
-            'addressId' => $addressId,
-            'isInDelivery' => $inDelivery,
-            'isValidated' => true
-        ]);
-        $orderId = $orderManager->createOne($order);
-
-        foreach ($cart as $key => $value) {
-            $orderProduct = new OrderProduct([
-                'orderId' => $orderId,
-                'productId' => $key,
-                'price' => $value['totalPrice'],
-                'quantity' => $value['quantity']
-            ]);
-            $orderProductManager->createOne($orderProduct);
-        }
-
-        $_SESSION['cart'] = [];
-        $_SESSION['inDelivery'] = false;
-
-        $_SESSION['status'] = "success";
-        $_SESSION['message'] = "Commande validée ! Vous avez payé " . $totalPrice . "€";
-        ob_clean();
-        header("location: index.php?page=homepage");
-        exit;
     }
-} else {
-    $_SESSION['status'] = "error";
-    $_SESSION['message'] = "Veuillez remplir tous les champs";
+
+    var_dump($_POST['coupon']);
+
+    $coupon = null;
+    if (!empty($_POST['coupon'])) {
+        $coupon = $couponManager->getOneByName($_POST['coupon']);
+
+        if ($coupon) {
+            $totalPrice -= $totalPrice * ($coupon['reduction'] / 100);
+        } else {
+            $_SESSION['status'] = "error";
+            $_SESSION['message'] = "Le coupon n'existe pas";
+            ob_clean();
+            header("location: index.php?page=cart");
+            exit;
+        }
+    }
+
+    $order = new Order([
+        'userId' => $_SESSION['user_id'],
+        'price' => $totalPrice,
+        'couponId' => $coupon ? $coupon['id'] : null,
+        'addressId' => $addressId,
+        'isInDelivery' => $inDelivery,
+        'isValidated' => true
+    ]);
+    $orderId = $orderManager->createOne($order);
+
+    foreach ($cart as $key => $value) {
+        $orderProduct = new OrderProduct([
+            'orderId' => $orderId,
+            'productId' => $key,
+            'price' => $value['totalPrice'],
+            'quantity' => $value['quantity']
+        ]);
+        $orderProductManager->createOne($orderProduct);
+    }
+
+    $_SESSION['cart'] = [];
+    $_SESSION['inDelivery'] = false;
+
+    $_SESSION['status'] = "success";
+    $_SESSION['message'] = "Commande validée ! Vous avez payé " . $totalPrice . "€";
     ob_clean();
-    header("location: index.php?page=cart");
+    header("location: index.php?page=homepage");
     exit;
 }
