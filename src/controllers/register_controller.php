ab40c1a5025021a97e32ef8533dd7882f5cdd441
Raxuis <raclot.raphael@gmail.com>
2024-01-30 09:27:52 +0100

    Feat(full) : directly logged when registered 🎉

--- src/controllers/register_controller.php
+++ src/controllers/register_controller.php
@@ -10,8 +10,6 @@ if (isset($_SESSION["user_id"])) {
 if (!empty($_POST)) {
     if (isset($_POST['token']) && $_POST['token'] == $_SESSION['token']) {
         if ($userManager->getOneByEmail($_POST['email']) == null && Verify::verifyPassword($_POST['password'], $_POST['password_confirmation']) && Verify::verifyFirstname($_POST['firstname']) && Verify::verifyLastname($_POST['lastname']) && Verify::verifyEmail($_POST['email'])) {
-            $_SESSION['status'] = "success";
-            $_SESSION['message'] = "Vous avez créé un compte avec succès";
             $user = new User([
                 'firstname' => $_POST['firstname'],
                 'lastname' => $_POST['lastname'],
@@ -19,8 +17,18 @@ if (!empty($_POST)) {
                 'password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
                 'isAdmin' => false,
             ]);
-
             $userId = $userManager->createOne($user);
+            $userInfos = $userManager->getOneByEmail($_POST['email']);
+            $_SESSION['status'] = "success";
+            $_SESSION['message'] = "Vous avez créé un compte avec succès";
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['name'] = $userInfos[0]['firstname'];
+            $_SESSION['user_id'] = $userInfos[0]['id'];
+            $_SESSION['is_admin'] = $userInfos[0]['is_admin'];
+            $_SESSION['logged'] = true;
+            ob_clean();
+            header('Location:index.php');
+            exit();
         } else {
             $_SESSION['status'] = "error";
             $_SESSION['message'] = "Les données saisies sont invalides ou un compte existe déjà avec cette email";
